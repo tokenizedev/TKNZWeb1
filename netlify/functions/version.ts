16245cfce701d1a298913f79b1a0fe84965c3296
web-mech <webmech@gmail.com>
2025-05-07 13:06:58 -0400

    feat(netlify): add CORS headers and handle OPTIONS request
    - Add CORS headers to allow all origins, specified headers, and methods
    - Handle OPTIONS request by returning status code 204 and headers
    
    BREAKING CHANGE: The function now handles OPTIONS requests by returning status code 204 and headers to enable cross-origin resource sharing.

--- netlify/functions/version.ts
+++ netlify/functions/version.ts
@@ -2,10 +2,24 @@ import { Handler } from '@netlify/functions'
 const { tknz } = require('../../package.json')
 
 export const handler: Handler = async (event) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 204,
+      headers
+    };
+  }
+
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify({
       ...tknz
     })
-  }
+  };
 }
