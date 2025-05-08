3af674186cd4866fa9ba3332d138487ee2aee5ea
web-mech <webmech@gmail.com>
2025-05-08 10:08:12 -0400

    feat: Add scheduled function to trigger updating leaderboard in the background

--- netlify/functions/trigger-update-leaderboard.js
+++ netlify/functions/trigger-update-leaderboard.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 export const config = { schedule: '*/10 * * * *' };
 
 export default async (_req, _res) => {
-  const response = await axios.post('https://tknz.fun/.netlify/functions/update-leaderboard-background', {
+  await axios.post('https://tknz.fun/.netlify/functions/update-leaderboard-background', {
     headers: {
       'Authorization': `Bearer ${process.env.WEBHOOK_SECRET}`,
     },
   });
-  return response.data;
 };
