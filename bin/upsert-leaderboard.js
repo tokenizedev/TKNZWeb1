01fbc904430f5c5414cbefe7a4a575be4bbf11e5
web-mech <webmech@gmail.com>
2025-05-22 13:30:43 -0400

    feat: Add dry-run flag handling in upsert-leaderboard script

--- bin/upsert-leaderboard.js
+++ bin/upsert-leaderboard.js
@@ -3,4 +3,18 @@ import { handler } from '../netlify/functions/update-leaderboard-background.js';
 
 dotenv.config();
 
-handler({}, {});
+// Parse dry-run flag from CLI args or environment
+const args = process.argv.slice(2);
+const dryRun = args.includes('--dry-run') || process.env.DRY_RUN === 'true';
+const event = { dryRun };
+
+(async () => {
+  try {
+    const res = await handler(event, {});
+    console.log('Handler response:', res);
+    process.exit(0);
+  } catch (err) {
+    console.error('Error running handler:', err);
+    process.exit(1);
+  }
+})();
