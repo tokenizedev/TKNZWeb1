7b5f5e756214a3acf218c32b7633f2a7ddba7d0a
web-mech <webmech@gmail.com>
2025-06-11 08:48:26 -0400

    fix: Use environment variable for Solana RPC URL in create-config script

--- scripts/create-config.sh
+++ scripts/create-config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+source .env
 
 ts-node bin/create-static-config.ts \
    --keypair ./config/keys/treasury.json \
-   --rpc https://mainnet.helius-rpc.com/?api-key=5e4edb76-36ed-4740-942d-7843adcc1e22 \
+   --rpc $SOLANA_RPC_URL \
    --index 0 \
    --pool-fees-file ./config/token/pool-fees.json \
    --sqrt-min-price 0 \
